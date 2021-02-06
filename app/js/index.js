193b22352eaaa5ff3002ece81b71a64f8cbdf1f8
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-06 12:34:40 +0000

    first commit

---
+++ app/js/index.js
@@ -0,0 +1,25 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", () => {
+  // Close hamburger menu
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-in");
+      elem.classList.add("fade-out");
+    });
+  } else {
+    //Open hamburger menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-out");
+      elem.classList.add("fade-in");
+    });
+  }
+});
